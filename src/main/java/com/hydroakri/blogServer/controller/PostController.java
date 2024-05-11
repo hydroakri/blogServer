996dd44b20cc8b98af09499bb41140f6345c0b48
hydroakri <hydroakri@protonmail.com>
2024-05-11 16:56:48 +0800

    finish view post

--- src/main/java/com/hydroakri/blogServer/controller/PostController.java
+++ src/main/java/com/hydroakri/blogServer/controller/PostController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -35,6 +36,7 @@ public class PostController {
         }
     }
 
+    @GetMapping
     public ResponseEntity<List<Post>> getAllPosts(){
         try{
             return ResponseEntity.status(HttpStatus.OK).body(postService.getAllPosts());
